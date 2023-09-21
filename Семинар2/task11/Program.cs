bde425bf89d0d58e48e58077fe9d65cae62d2c9c
Danila <kudasovdd@gmail.com>
2023-09-21 21:54:11 +0300

    Seminar2

---
+++ Семинар2/task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int lastDigit = num % 10;
+int firstDigit = num / 100;
+int result = firstDigit * 10 + lastDigit;
+
+Console.WriteLine(result);
---
+++ Семинар2/task9/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = Random.Shared.Next(10, 100);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+int firstDigit = number / 10;
+
+if (firstDigit > lastDigit)
+{
+    Console.WriteLine($"Первая цифра больше второй. Наибольшая цифра = {firstDigit}");
+}
+else if(firstDigit < lastDigit)
+{
+    Console.WriteLine($"Вторая цифра больше первой. Наибольшая цифра = {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Цифры равны");
+}
